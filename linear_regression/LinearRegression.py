Fixed issue with Loss gradient computations, using y vs. batched y

linear_regression/LinearRegression.py
@@ -11,25 +11,24 @@ class LinearRegression:
         self.batched_y = np.array_split(self.y, np.arange(self.batch_size, len(self.y), self.batch_size))
 
         self.w = np.random.randn() * 0.0001
-        self.b = np.random.randn() *0.001
+        self.b = np.random.randn() * 0.001
 
-        avg_batch_loss = []
         
         for i in range(len(self.batched_X)):
             pred = self.predict(self.batched_X[i])
-            loss = np.sum(np.power(pred - self.y[i], 2)/2)/self.batch_size
-            loss_w_grad = 2 * (pred - self.y[i]) * self.batched_X[i]
-            loss_b_grad = 2 * (pred - self.y[i])
+            loss = np.mean(np.power(pred - self.batched_y[i], 2)/2)
+            loss_w_grad = 2 * (pred - self.batched_y[i]) * self.batched_X[i]
+            loss_b_grad = 2 * (pred - self.batched_y[i])
+
 
             loss_w_mean_grad = np.mean(loss_w_grad)
             loss_b_mean_grad = np.mean(loss_b_grad)
 
-            self.w -= lr * loss_w_mean_grad
-            self.b -= lr * loss_b_mean_grad
+            self.w -= (lr * loss_w_mean_grad)
+            self.b -= (lr * loss_b_mean_grad)
 
-            avg_batch_loss.append(loss)
             print(f"Avg. Loss in Batch: {loss:.5f} [{i+1}/{len(self.batched_X)} { '=' * round( 50 * ( i/len(self.batched_X[i]) ) ) }]", end='\r')
-    
+
         return loss
     
 